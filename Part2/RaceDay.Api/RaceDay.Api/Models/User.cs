21c7faed110571d2228b3dd3c33058d63064de75
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-09-23 10:10:33 +0200

    Added a class called User and its information

---
+++ Part2/RaceDay.Api/RaceDay.Api/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaceDay.Api.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required, MaxLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required, MaxLength(20)]
+        public string Role { get; set; } = string.Empty; // "Organiser" or "Participant"
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public ICollection<Event>? Events { get; set; }
+        public ICollection<Enrolment>? Enrolments { get; set; }
+
+        public ICollection<Result>? CapturedResults { get; set; }
+
+
+
+    }
+}
