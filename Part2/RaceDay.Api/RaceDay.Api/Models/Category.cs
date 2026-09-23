45dcbcb53ed284cc4d66d0be677e22734b420079
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-09-23 10:08:51 +0200

    Added a class called category and its information

---
+++ Part2/RaceDay.Api/RaceDay.Api/Models/Category.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaceDay.Api.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+
+        [ForeignKey("EventId")]
+        public Event? Event { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        public int MaxParticipants { get; set; } = 100;
+
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal EntryFee { get; set; } = 0;
+
+        public ICollection<Enrolment>? Enrolments { get; set; }
+
+
+    }
+}
