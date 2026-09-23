cd5e86b81257fe08a38154320f21e8a7b5931a93
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-09-23 10:10:56 +0200

    Added a class called Venue and its information

---
+++ Part2/RaceDay.Api/RaceDay.Api/Models/Venue.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaceDay.Api.Models
+{
+    public class Venue
+    {
+        [Key]
+        public int VenueId { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+
+        [ForeignKey("EventId")]
+        public Event? Event { get; set; }
+
+        [Required, MaxLength(200)]
+        public string Address { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string City { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(9,6)")]
+        public decimal? Latitude { get; set; }
+
+        [Column(TypeName = "decimal(9,6)")]
+        public decimal? Longitude { get; set; }
+
+    }
+}
