81421ecec4c196c4c4a5aba931023fb818dd7c99
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-09-23 10:11:27 +0200

    Added more information on the class called information

--- Part2/RaceDay.Api/RaceDay.Api/Program.cs
+++ Part2/RaceDay.Api/RaceDay.Api/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using RaceDay.Api.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<RaceDayDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RaceDayDb")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 
@@ -7,6 +16,11 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
