1b0989fc79c27b3ebded11c0a5f7979b4e058243
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-09-23 10:09:17 +0200

    Added a class called enrolment and its information

---
+++ Part2/RaceDay.Api/RaceDay.Api/Models/Enrolment.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaceDay.Api.Models
+{
+    public class Enrolment
+    {
+        [Key]
+        public int EnrolmentId { get; set; }
+
+        [Required]
+        public int ParticipantId { get; set; }
+
+        [ForeignKey("ParticipantId")]
+        public User? Participant { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+
+        public DateTime EnrolmentDate { get; set; } = DateTime.Now;
+
+        [MaxLength(20)]
+        public string Status { get; set; } = "Active";
+
+        public Result? Result { get; set; }
+
+    }
+}
