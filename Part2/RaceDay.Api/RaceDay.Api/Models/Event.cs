a1d519caffcbae360c5d7c59d4e64409cc0ec30f
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-09-23 10:09:40 +0200

    Added a class called Event and its information

---
+++ Part2/RaceDay.Api/RaceDay.Api/Models/Event.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaceDay.Api.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [Required]
+        public int OrganiserId { get; set; }
+
+        [ForeignKey("OrganiserId")]
+        public User? Organiser { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime EventDate { get; set; }
+
+        [Required, Column(TypeName = "decimal(5,2)")]
+        public decimal DistanceKm { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Status { get; set; } = "Upcoming";
+
+        public ICollection<Category>? Categories { get; set; }
+        public ICollection<Venue>? Venues { get; set; }
+
+    }
+}
