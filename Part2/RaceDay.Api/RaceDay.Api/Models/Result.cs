0b1356212bfefb3a74ac732cec2cc64c723280f0
Onthatile-015 <ST10486338@rcconnect.edu.za>
2026-09-23 10:10:08 +0200

    Added a class called Result and its information

---
+++ Part2/RaceDay.Api/RaceDay.Api/Models/Result.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaceDay.Api.Models
+{
+    public class Result
+    {
+        [Key]
+        public int ResultId { get; set; }
+
+        [Required]
+        public int EnrolmentId { get; set; }
+
+        [ForeignKey("EnrolmentId")]
+        public Enrolment? Enrolment { get; set; }
+
+        public TimeSpan? FinishTime { get; set; }
+
+        public int? Position { get; set; }
+
+        [Required]
+        public int CapturedByOrganiserId { get; set; }
+
+        [ForeignKey("CapturedByOrganiserId")]
+        public User? CapturedByOrganiser { get; set; }
+
+
+    }
+}
